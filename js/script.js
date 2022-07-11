0c3395b29e94929f49565c7ada64fd1dccabab8a
Ian Bortnic <ianbortnic.uchile@gmail.com>
2022-07-11 08:32:43 -0400

    junte recetas predefinidas y nuevas

--- js/script.js
+++ js/script.js
@@ -2,29 +2,27 @@ const btnRecetasTodas = document.querySelector('#recetas_todas');
 const btnRecetasDulces = document.querySelector('#recetas_dulces');
 const btnRecetasSaladas = document.querySelector('#recetas_saladas');
 
-const btnPropiasRecetas = document.querySelector('#propias_recetas');
 
 const contenedor = document.querySelector('#contenedorTarjetas');
 const container = document.querySelector('#cardContainer');
 const selectTipoReceta = document.querySelector('#tipo_receta');
 const searchBtn = document.querySelector('#search');
 
-// let ian = {'ian':{'cantidad': 200}, 'max':{'cantidad': 100}};
 
-function get_cantidad_ingredientes(dic){
+function get_cantidad_ingredientes(dic) {
     let final_string = '';
-    for([key, val] of Object.entries(dic)) {
+    for ([key, val] of Object.entries(dic)) {
         final_string += `${key} (${val.cantidad} gr), `;
-      }
+    }
     return final_string;
 }
 
-function get_informacion_nutricional_receta(dic){
+function get_informacion_nutricional_receta(dic) {
 
     var values = [];
-    for([key, val] of Object.entries(dic)) {
+    for ([key, val] of Object.entries(dic)) {
         values.push(val);
-      }
+    }
 
     console.log(values);
     let total_calorias = values.reduce((acc, ing) => acc + ing['calorias'] * ing['cantidad'], 0);
@@ -33,24 +31,22 @@ function get_informacion_nutricional_receta(dic){
     let total_grasa = values.reduce((acc, ing) => acc + ing['grasas'] * ing['cantidad'], 0);
     let total_cantidad = values.reduce((acc, ing) => acc + ing['cantidad'], 0);
 
-    let calorias_per_100_gr = Math.round(total_calorias / total_cantidad,1);
-    let proteinas_per_100_gr = Math.round(total_proteinas / total_cantidad,1);
-    let carbohidratos_per_100_gr = Math.round(total_carbohidratos / total_cantidad,1);
-    let grasa_per_100_gr = Math.round(total_grasa / total_cantidad,1);
+    let calorias_per_100_gr = Math.round(total_calorias / total_cantidad, 1);
+    let proteinas_per_100_gr = Math.round(total_proteinas / total_cantidad, 1);
+    let carbohidratos_per_100_gr = Math.round(total_carbohidratos / total_cantidad, 1);
+    let grasa_per_100_gr = Math.round(total_grasa / total_cantidad, 1);
 
     let final_string = `calorias: (${calorias_per_100_gr}), proteinas: (${proteinas_per_100_gr} gr.), carbohidratos: (${carbohidratos_per_100_gr} gr.), grasas: (${grasa_per_100_gr} gr.).`
 
-    return final_string;
+    return [final_string, calorias_per_100_gr];
 
 
 }
 
 
 
-
-
-class Receta{
-    constructor(imagen, nombre_receta, tipo_receta, ingredientes, informacion_nutricional, preparacion){
+class Receta {
+    constructor(imagen, nombre_receta, tipo_receta, ingredientes, informacion_nutricional, preparacion) {
         this.imagen = imagen;
         this.nombre_receta = nombre_receta;
         this.tipo_receta = tipo_receta
@@ -61,7 +57,7 @@ class Receta{
 }
 
 
-function guardarReceta(receta){
+function guardarReceta(receta) {
     json_receta = JSON.stringify({
         "imagen": receta.imagen,
         // "imagen": "../img_recetas/tiramisu.jpeg",
@@ -77,11 +73,11 @@ function guardarReceta(receta){
 }
 
 
-let maximo_calorias = 400;
+let maximo_calorias_per_100_g = 400;
 
 let btnIngresarNombre = document.getElementById('Ingresar Nombre');
 
-btnIngresarNombre.addEventListener('click', ()=>{
+btnIngresarNombre.addEventListener('click', () => {
     let nombre_receta = document.getElementById('nombre_receta').value;
     Swal.fire({
         icon: 'success',
@@ -93,18 +89,17 @@ btnIngresarNombre.addEventListener('click', ()=>{
 
 let btnIngresarIngrediente = document.getElementById('Ingresar Ingrediente');
 
-let new_ingredientes =  {};
+let new_ingredientes = {};
 
-btnIngresarIngrediente.addEventListener('click', ()=>{
+btnIngresarIngrediente.addEventListener('click', () => {
     let nombre_ingrediente = document.getElementById('nombre_ingrediente').value;
     let cantidad_ingrediente = parseInt(document.getElementById('cantidad_ingrediente').value);
-    if(isNaN(cantidad_ingrediente)  || cantidad_ingrediente<=0){
+    if (isNaN(cantidad_ingrediente) || cantidad_ingrediente <= 0) {
         Swal.fire({
             icon: 'error',
             text: 'Debes ingresar una cantidad valida',
         })
-    }
-    else{
+    } else {
         const options = {
             method: 'GET',
             headers: {
@@ -116,13 +111,20 @@ btnIngresarIngrediente.addEventListener('click', ()=>{
         fetch(`https://calorieninjas.p.rapidapi.com/v1/nutrition?query=${nombre_ingrediente}`, options)
             .then(response => response.json())
             .then(response => {
-                if(response.items.length>0){
+                if (response.items.length > 0) {
                     // console.log(response.items[0].name);
                     let info = response.items[0];
-                    new_ingredientes[nombre_ingrediente] = {'info': info,'proteinas':info.protein_g, 'carbohidratos':info.carbohydrates_total_g,'grasas': info.fat_total_g,'calorias': info.calories,  'cantidad': cantidad_ingrediente};
+                    new_ingredientes[nombre_ingrediente] = {
+                        'info': info,
+                        'proteinas': info.protein_g,
+                        'carbohidratos': info.carbohydrates_total_g,
+                        'grasas': info.fat_total_g,
+                        'calorias': info.calories,
+                        'cantidad': cantidad_ingrediente
+                    };
                     console.log(new_ingredientes);
 
-                }else{
+                } else {
                     Swal.fire({
                         icon: 'error',
                         text: 'Lo sentimos, no pudimos encontrar ese ingrediente.',
@@ -131,49 +133,86 @@ btnIngresarIngrediente.addEventListener('click', ()=>{
                 }
 
             })
-            .then(document.getElementById('nombre_ingrediente').value='')
-            .then(document.getElementById('cantidad_ingrediente').value='')
-        }
-    })
+            .then(document.getElementById('nombre_ingrediente').value = '')
+            .then(document.getElementById('cantidad_ingrediente').value = '')
+    }
+})
 
 
 let btnIngresarReceta = document.getElementById('Ingresar Receta');
 
 
-btnIngresarReceta.addEventListener('click', ()=>{
-    console.log(new_ingredientes);
-    let imagen = document.getElementById('imagen_receta').value;
-    let tipo_receta = document.getElementById('tipo_receta').value;
+btnIngresarReceta.addEventListener('click', () => {
+
     let nombre_receta = document.getElementById('nombre_receta').value;
-    let ingredientes = get_cantidad_ingredientes(new_ingredientes);
-    let informacion_nutricional = get_informacion_nutricional_receta(new_ingredientes);
-    let preparacion = document.getElementById('preparacion').value;
 
-
-    let receta = new Receta(imagen, nombre_receta, tipo_receta, ingredientes, informacion_nutricional, preparacion);
-    // total_calorias = new_ingredientes.reduce((acc, ing) => acc + ing.calories, 0);
-    total_calorias = 100;
-    if(total_calorias==0){
+    if (nombre_receta == '') {
         Swal.fire({
             icon: 'error',
-            text: 'No puede ingresar una receta vacía.  ',
+            text: 'Debe Ingresar un nombre para su receta',
         })
-    }
-    else{
-        if(total_calorias > maximo_calorias){
+
+    } else {
+        let imagen = document.getElementById('imagen_receta').value;
+
+        if (imagen == '') {
             Swal.fire({
                 icon: 'error',
-                title: `${nombre_receta} no pudo ser ingresada.`,
-                text: `Excede el máximo de calorías (${maximo_calorias})! (tiene un total de ${total_calorias} calorías)`,
-            })}
-            else{
+                text: 'Debe subir una imagen de su receta',
+            })
+
+        } else {
+            let preparacion = document.getElementById('preparacion').value;
+            if (preparacion == '') {
                 Swal.fire({
-                    icon: 'success',
-                    title: `${nombre_receta} ha sido ingresada con éxito!`,
+                    icon: 'error',
+                    text: 'Debe detallar la preparacion de su receta',
                 })
-                guardarReceta(receta);
+
+            } else {
+
+                let tipo_receta = document.getElementById('tipo_receta').value;
+
+                let ingredientes = get_cantidad_ingredientes(new_ingredientes);
+                let [informacion_nutricional, calorias_per_100_gr] = get_informacion_nutricional_receta(new_ingredientes);
+
+
+
+                let receta = new Receta(imagen, nombre_receta, tipo_receta, ingredientes, informacion_nutricional, preparacion);
+                // total_calorias = new_ingredientes.reduce((acc, ing) => acc + ing.calories, 0);
+
+                if (calorias_per_100_gr == 0) {
+                    Swal.fire({
+                        icon: 'error',
+                        text: 'No puede ingresar una receta vacía.  ',
+                    })
+                } else {
+                    if (calorias_per_100_gr > maximo_calorias_per_100_g) {
+                        Swal.fire({
+                            icon: 'error',
+                            title: `${nombre_receta} no pudo ser ingresada.`,
+                            text: `Excede el máximo de calorías c/ 100 gr. (${maximo_calorias_per_100_g})! (tiene un total de ${calorias_per_100_gr} calorías c/ 100 gr.)`,
+                        })
+                    } else {
+                        Swal.fire({
+                            icon: 'success',
+                            title: `${nombre_receta} ha sido ingresada con éxito!`,
+                        })
+                        guardarReceta(receta);
+                    }
+                }
+
             }
+
+        }
+
+
+
+
+
     }
+
+
 
 })
 
@@ -202,10 +241,10 @@ function createHTML(array) {
 
 
 
-function filtrarRecetas(recetas, tipo_receta){
-    if(tipo_receta=='Todas'){
+function filtrarRecetas(recetas, tipo_receta) {
+    if (tipo_receta == 'Todas') {
         return recetas
-    }else{
+    } else {
         const recetas_filtradas = recetas.filter(receta => receta.tipo_receta == tipo_receta);
         return recetas_filtradas;
     }
@@ -217,7 +256,19 @@ function filtrarRecetas(recetas, tipo_receta){
 async function traerRecetas(tipo_receta) {
     const respuesta = await fetch('../js/data.json');
     const data = await respuesta.json();
-    createHTML(filtrarRecetas(data, tipo_receta));
+
+    let propias_recetas = [];
+
+    for (let i = 0; i < localStorage.length; i++) {
+        let nombre_receta = localStorage.key(i);
+        receta = localStorage.getItem(nombre_receta);
+        propias_recetas.push(JSON.parse(receta));
+    }
+
+    todas_las_recetas = propias_recetas.concat(data)
+
+    recetas_filtradas = filtrarRecetas(todas_las_recetas, tipo_receta)
+    createHTML(recetas_filtradas);
 }
 
 btnRecetasTodas.addEventListener('click', () => {
@@ -228,18 +279,4 @@ btnRecetasDulces.addEventListener('click', () => {
 })
 btnRecetasSaladas.addEventListener('click', () => {
     traerRecetas("Salada");
-})
-
-
-btnPropiasRecetas.addEventListener('click', () => {
-
-    let propias_recetas = [];
-
-    for(let i=0; i<localStorage.length; i++){
-        let nombre_receta = localStorage.key(i);
-        receta = localStorage.getItem(nombre_receta);
-        propias_recetas.push(JSON.parse(receta));
-    }
-    console.log(propias_recetas);
-    createHTML(propias_recetas);
 })
