b6427e300f44c9be3f14738d3bb6c029d72ddc22
Ian Bortnic <ianbortnic.uchile@gmail.com>
2022-07-12 11:12:11 -0400

    codigo mas ordenado

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-const btnRecetasTodas = document.querySelector('#recetas_todas');
-const btnRecetasDulces = document.querySelector('#recetas_dulces');
-const btnRecetasSaladas = document.querySelector('#recetas_saladas');
-
-
-const contenedor = document.querySelector('#contenedorTarjetas');
-const container = document.querySelector('#cardContainer');
+class Receta {
+    constructor(imagen, nombre_receta, tipo_receta, ingredientes, informacion_nutricional, preparacion) {
+        this.nombre_receta = nombre_receta;
+        this.tipo_receta = tipo_receta
+        this.ingredientes = ingredientes
+        this.informacion_nutricional = informacion_nutricional
+        this.preparacion = preparacion
+        this.imagen = imagen
+    }
+}
 
 
 function get_cantidad_ingredientes(dic) {
@@ -41,17 +44,6 @@ function get_informacion_nutricional_receta(dic) {
 }
 
 
-class Receta {
-    constructor(imagen, nombre_receta, tipo_receta, ingredientes, informacion_nutricional, preparacion) {
-        this.nombre_receta = nombre_receta;
-        this.tipo_receta = tipo_receta
-        this.ingredientes = ingredientes
-        this.informacion_nutricional = informacion_nutricional
-        this.preparacion = preparacion
-        this.imagen = imagen
-    }
-}
-
 
 function guardarReceta(receta) {
     json_receta = JSON.stringify({
@@ -68,9 +60,77 @@ function guardarReceta(receta) {
 }
 
 
-let maximo_calorias_per_100_g = 400;
+function createHTML(array) {
+    contenedor.innerHTML = '';
+    container.innerHTML = '';
+    array.forEach((receta) => {
+        const tarjeta = `
+            <div class="col" style="margin-bottom: 50px;">
+                <div class="card h-100">
+                    <img src="${receta.imagen}" class="card-img-top" alt="${receta.nombre_receta}">
+                    <div class="card-body black_title" style="height: 300px;  overflow: scroll">
+                        <p class="card-text" style="color: black"><b> Nombre:</b> ${receta.nombre_receta}</p>
+                        <p class="card-text" style="color: black"><b>Información Nutricional (c/ 100 gr.):</b> ${receta.informacion_nutricional}</p>
+                        <p class="card-text" style="color: black"><b>Ingredientes:</b> ${receta.ingredientes}</p>
+                        <p class="card-text" style="color: black"><b>Preparación:</b> ${receta.preparacion}</p>
+                    </div>
+                </div>
+            </div>`;
+        contenedor.innerHTML += tarjeta;
+    })
+}
+
+
+function filtrarRecetas(recetas, tipo_receta) {
+    if (tipo_receta == 'Todas') {
+        return recetas
+    } else {
+        const recetas_filtradas = recetas.filter(receta => receta.tipo_receta == tipo_receta);
+        return recetas_filtradas;
+    }
+
+}
+
+
+
+async function traerRecetas(tipo_receta) {
+    const respuesta = await fetch('../js/data.json');
+    const data = await respuesta.json();
+
+    let propias_recetas = [];
+
+    for (let i = 0; i < localStorage.length; i++) {
+        let nombre_receta = localStorage.key(i);
+        receta = localStorage.getItem(nombre_receta);
+        propias_recetas.push(JSON.parse(receta));
+
+    }
+
+    todas_las_recetas = propias_recetas.concat(data)
+
+    recetas_filtradas = filtrarRecetas(todas_las_recetas, tipo_receta)
+    createHTML(recetas_filtradas);
+}
+
+
+const btnRecetasTodas = document.querySelector('#recetas_todas');
+const btnRecetasDulces = document.querySelector('#recetas_dulces');
+const btnRecetasSaladas = document.querySelector('#recetas_saladas');
+
+const contenedor = document.querySelector('#contenedorTarjetas');
+const container = document.querySelector('#cardContainer');
+
 
 let btnIngresarNombre = document.getElementById('Ingresar Nombre');
+
+let btnIngresarIngrediente = document.getElementById('Ingresar Ingrediente');
+
+let btnIngresarReceta = document.getElementById('Ingresar Receta');
+
+let new_ingredientes = {};
+
+let maximo_calorias_per_100_g = 400;
+
 
 btnIngresarNombre.addEventListener('click', () => {
     let nombre_receta = document.getElementById('nombre_receta').value;
@@ -80,11 +140,6 @@ btnIngresarNombre.addEventListener('click', () => {
         text: 'ya me dio hambre...',
     })
 })
-
-
-let btnIngresarIngrediente = document.getElementById('Ingresar Ingrediente');
-
-let new_ingredientes = {};
 
 btnIngresarIngrediente.addEventListener('click', () => {
     let nombre_ingrediente = document.getElementById('nombre_ingrediente').value;
@@ -132,7 +187,7 @@ btnIngresarIngrediente.addEventListener('click', () => {
 })
 
 
-let btnIngresarReceta = document.getElementById('Ingresar Receta');
+
 
 
 btnIngresarReceta.addEventListener('click', () => {
@@ -219,63 +274,6 @@ btnIngresarReceta.addEventListener('click', () => {
 
 
 })
-
-
-
-function createHTML(array) {
-    contenedor.innerHTML = '';
-    container.innerHTML = '';
-    array.forEach((receta) => {
-        const tarjeta = `
-            <div class="col" style="margin-bottom: 50px;">
-                <div class="card h-100">
-                    <img src="${receta.imagen}" class="card-img-top" alt="${receta.nombre_receta}">
-                    <div class="card-body black_title" style="height: 300px;  overflow: scroll">
-                        <p class="card-text" style="color: black"><b> Nombre:</b> ${receta.nombre_receta}</p>
-                        <p class="card-text" style="color: black"><b>Información Nutricional (c/ 100 gr.):</b> ${receta.informacion_nutricional}</p>
-                        <p class="card-text" style="color: black"><b>Ingredientes:</b> ${receta.ingredientes}</p>
-                        <p class="card-text" style="color: black"><b>Preparación:</b> ${receta.preparacion}</p>
-                    </div>
-                </div>
-            </div>`;
-        contenedor.innerHTML += tarjeta;
-    })
-}
-
-
-
-
-function filtrarRecetas(recetas, tipo_receta) {
-    if (tipo_receta == 'Todas') {
-        return recetas
-    } else {
-        const recetas_filtradas = recetas.filter(receta => receta.tipo_receta == tipo_receta);
-        return recetas_filtradas;
-    }
-
-}
-
-
-
-async function traerRecetas(tipo_receta) {
-    const respuesta = await fetch('../js/data.json');
-    const data = await respuesta.json();
-
-    let propias_recetas = [];
-
-    for (let i = 0; i < localStorage.length; i++) {
-        let nombre_receta = localStorage.key(i);
-        receta = localStorage.getItem(nombre_receta);
-        propias_recetas.push(JSON.parse(receta));
-
-    }
-
-    todas_las_recetas = propias_recetas.concat(data)
-
-    recetas_filtradas = filtrarRecetas(todas_las_recetas, tipo_receta)
-    createHTML(recetas_filtradas);
-}
-
 
 
 
